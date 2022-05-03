3679e8a54ce4b7bcd624941c26671566121c2295
Ron Heft <ron.heft@cms.hhs.gov>
2022-05-03 12:13:17 -0400

    feat: Initial home questions

--- packages/sba-eligibility/src/flow/content/questions.flow.ts
+++ packages/sba-eligibility/src/flow/content/questions.flow.ts
@@ -47,8 +47,108 @@ const HOME_C: Partial<IQuestion> = {
   title:    'Was the space your primary home when the event happened?',
 };
 
+/**
+ * Home: zip code
+ */
+const HOME_D: Partial<IQuestion> = {
+  answers: [YES, NO],
+  id:      'HOME_D',
+  info:    'Five-digit ZIP Code',
+  title:   'What\'s the ZIP Code for your home?',
+};
+
+/**
+ * Home: disaster events
+ */
+const HOME_E: Partial<IQuestion> = {
+  answers:  [YES, NO],
+  id:       'HOME_E',
+  subTitle: 'ZIP Code: 42101',
+  title:    'Looks like there were a few events where you live. Which one(s) affected you?',
+};
+
+/**
+ * Home: loan usage - own
+ */
+const HOME_FA: Partial<IQuestion> = {
+  answers: [YES, NO],
+  id:      'HOME_FA',
+  title:   'A loan for the sudden event you selected must be used to repair your home and other items like furniture, clothing, cars, and appliances. Did the event cause physical damage to your home?',
+};
+
+/**
+ * Home: loan usage - rent
+ */
+const HOME_FB: Partial<IQuestion> = {
+  answers: [YES, NO],
+  id:      'HOME_FB',
+  title:   'A loan for the sudden event you selected must be used to replace furniture, clothing, cars, and other items that were damaged. Did the event cause physical damage to your personal stuff?',
+};
+
+/**
+ * Home: age
+ */
+const HOME_G: Partial<IQuestion> = {
+  answers:  [YES, NO],
+  id:       'HOME_G',
+  subTitle: 'The legal age to borrow money varies based on where you live.',
+  title:    'How old are you?',
+};
+
+/**
+ * Home: people living at home
+ */
+const HOME_H: Partial<IQuestion> = {
+  answers: [YES, NO],
+  id:      'HOME_H',
+  info:    'Number of kids and adults in your home',
+  title:   'How many people lived in your home when the sudden event happened?',
+};
+
+/**
+ * Home: people depending on income
+ */
+const HOME_I: Partial<IQuestion> = {
+  answers:  [YES, NO],
+  id:       'HOME_I',
+  info:     'Number of kids and adults who depend on the same income',
+  subTitle: 'You may have roommates, friends, extended family, or others who share your space but don\'t live off of the same income as you.',
+  title:    'How many of those people depend on the same income?',
+};
+
+/**
+ * Home: income
+ */
+const HOME_J: Partial<IQuestion> = {
+  answers:  [YES, NO],
+  id:       'HOME_J',
+  info:     'Estimate for total combined monthly income',
+  subTitle: 'Estimate the total amount for everyone who depends on the same income in your home. Include money from jobs and government assistance (Social Security benefits, unemployment insurance, etc.).',
+  title:    'How much money do you make every month?',
+};
+
+/**
+ * Home: debt
+ */
+const HOME_K: Partial<IQuestion> = {
+  answers:  [YES, NO],
+  id:       'HOME_K',
+  info:     'Estimate for total combined debt',
+  subTitle: 'Estimate the total amount for everyone who depends on the same income in your home. Debt includes things like car or student loans, credit card payments, and mortgages.',
+  title:    'How much debt do you pay back every month?',
+};
+
 export const questionContentMap: TQuestionMap = {
   A,
   HOME_B,
   HOME_C,
+  HOME_D,
+  HOME_E,
+  HOME_FA,
+  HOME_FB,
+  HOME_G,
+  HOME_H,
+  HOME_I,
+  HOME_J,
+  HOME_K,
 };
